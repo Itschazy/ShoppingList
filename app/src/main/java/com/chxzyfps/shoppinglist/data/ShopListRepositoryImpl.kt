a5de4d61df939f57fcc256590fe8cd6f8b5b820e
Eugeny Chudov <eschudov@gmail.com>
2023-03-10 11:37:05 +0300

    Added scrollview with linearlayout

--- app/src/main/java/com/chxzyfps/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/chxzyfps/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -9,9 +9,16 @@ import com.chxzyfps.shoppinglist.domain.ShopListRepository
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLiveData = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
 
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0..1000) {
+            val item = ShopItem("Name $i", i, true)
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == UNDEFINED_ID) {

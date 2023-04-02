c3d735bc4d766c4ef4dad2cf88c6089a40c06282
Eugeny Chudov <eschudov@gmail.com>
2023-04-02 15:41:49 +0300

    Adding simple content provider

--- app/src/main/java/com/chxzyfps/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/chxzyfps/shoppinglist/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.chxzyfps.shoppinglist.presentation
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -47,6 +48,14 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedList
                 startActivity(intent)
             }
         }
+        contentResolver.query(
+            Uri.parse("content://com.chxzyfps.shoppinglist/shop_items"),
+            null,
+            null,
+            null,
+            null,
+            null
+        )
 
     }
 
